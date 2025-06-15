5e0a97bddfe8d7e965345b5ede50d69c0ee15f36
alex-roofengine <80418853+alex-roofengine@users.noreply.github.com>
2025-06-16 05:59:13 +0800

    Update create-setup-intent.js

--- netlify/functions/create-setup-intent.js
+++ netlify/functions/create-setup-intent.js
@@ -1,17 +1,7 @@
-require('dotenv').config();
+// For updating existing customer
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-
-exports.handler = async (event) => {
-  const { customerId } = JSON.parse(event.body);
-  if (!customerId) return { statusCode: 400, body: 'Missing customerId' };
-
-  const intent = await stripe.setupIntents.create({
-    customer: customerId,
-    usage: 'off_session',
-    payment_method_types: ['card'],
-  });
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ clientSecret: intent.client_secret }),
-  };
+exports.handler = async ({ body }) => {
+  const { customerId } = JSON.parse(body);
+  const intent = await stripe.setupIntents.create({ customer: customerId, usage: 'off_session', payment_method_types: ['card'] });
+  return { statusCode: 200, body: JSON.stringify({ clientSecret: intent.client_secret }) };
 };
