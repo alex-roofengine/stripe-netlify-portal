2e48ba90c99aea480017c8eb2b9db2528c3254c9
alex-roofengine <80418853+alex-roofengine@users.noreply.github.com>
2025-06-16 07:44:20 +0800

    Update list-subscriptions.js

--- netlify/functions/list-subscriptions.js
+++ netlify/functions/list-subscriptions.js
@@ -1,13 +1,6 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 exports.handler = async ({ queryStringParameters }) => {
   const { customerId } = queryStringParameters;
-  // Expand plan.product to get product name
-  const subs = await stripe.subscriptions.list({
-    customer: customerId,
-    expand: ['data.plan.product']
-  });
-  return {
-    statusCode: 200,
-    body: JSON.stringify(subs.data)
-  };
+  const subs = await stripe.subscriptions.list({ customer: customerId, expand: ['data.plan.product'] });
+  return { statusCode: 200, body: JSON.stringify(subs.data) };
 };
