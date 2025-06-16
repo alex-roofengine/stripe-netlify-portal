47a935d573e6dc0c8207be467237249cfe3bd297
alex-roofengine <80418853+alex-roofengine@users.noreply.github.com>
2025-06-16 08:34:44 +0800

    Update list-payment-methods.js

--- netlify/functions/list-payment-methods.js
+++ netlify/functions/list-payment-methods.js
@@ -1,6 +1,24 @@
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-exports.handler = async ({ queryStringParameters }) => {
-  const { customerId } = queryStringParameters;
-  const pms = await stripe.paymentMethods.list({ customer: customerId, type: 'card' });
-  return { statusCode: 200, body: JSON.stringify(pms.data) };
+// netlify/functions/list-payment-methods.js
+const Stripe = require('stripe');
+const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
+
+exports.handler = async function(event) {
+  const { customerId } = event.queryStringParameters;
+
+  // 1. List all card PMs
+  const pmList = await stripe.paymentMethods.list({
+    customer: customerId,
+    type: 'card',
+  });
+
+  // 2. Fetch the customer so we know the default PM
+  const customer = await stripe.customers.retrieve(customerId);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      paymentMethods: pmList.data,
+      defaultPaymentMethod: customer.invoice_settings.default_payment_method
+    })
+  };
 };
