f960eff90533024b25a668d153136bd39d5caf1f
alex-roofengine <80418853+alex-roofengine@users.noreply.github.com>
2025-06-16 10:21:39 +0800

    Update list-subscriptions.js

--- netlify/functions/list-subscriptions.js
+++ netlify/functions/list-subscriptions.js
@@ -1,6 +1,26 @@
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-exports.handler = async ({ queryStringParameters }) => {
-  const { customerId } = queryStringParameters;
-  const subs = await stripe.subscriptions.list({ customer: customerId, expand: ['data.plan.product'] });
-  return { statusCode: 200, body: JSON.stringify(subs.data) };
+// netlify/functions/list-subscriptions.js
+const Stripe = require('stripe');
+const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
+
+exports.handler = async (event) => {
+  try {
+    const { customerId } = event.queryStringParameters;
+
+    // List all subscriptions (active, canceled, paused, etc.)
+    const subscriptions = await stripe.subscriptions.list({
+      customer: customerId,
+      status: 'all',
+      expand: ['data.plan.product'],
+    });
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(subscriptions.data),
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: err.message }),
+    };
+  }
 };
